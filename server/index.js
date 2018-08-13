fba2bd0e7935bbcbc7095f64955013ab573db378
isurusiri <isuruanatomy@gmail.com>
2018-08-13 20:34:23 +0530

    rethink timer query

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const io = require('socket.io')();
+const r = require('rethinkdb');
 
-io.on('connection', (client) => {
-    client.on('subscribeToTimer', (interval) => {
-        console.log('client is subscribing to timer with interval ', interval);
-        setInterval(() => {
-            client.emit('timer', new Date());
-        }, interval);
+r.connect({
+    host: 'localhost',
+    port: 28015,
+    db: 'awesome_whiteboard',
+}).then((connection) => {
+    io.on('connection', (client) => {
+        client.on('subscribeToTimer', (interval) => {
+            console.log('client is subscribing to timer with interval ', interval);
+            r.table('timers')
+            .changes()
+            .run(connection)
+            .then((corsur) => {
+                corsur.each((err, timerRow) => {
+                    client.emit('timer', timerRow.new_val.timestamp);
+                })
+            });
+        });
     });
 });
 
